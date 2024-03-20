a0d5d1d50f01200907bcc5e18a4d27827f1dd3ab
Gerald Behen <s3761521@students.fscj.edu>
2024-03-20 15:30:02 -0400

    Add files via upload
    JavaFX Documentation Project. I have been testing these programs to see if there are any issues that need attention and provide documentation.

---
+++ transformapp/EmploymentRequestModel.java
@@ -0,0 +1,9 @@
+package com.example.transformapp;
+
+public class EmploymentRequestModel {
+    public void save(EmploymentRequest req) {
+        System.out.println("saving " + req);
+    }
+}
+
+
---
+++ transformapp/TransformApp.java
@@ -0,0 +1,34 @@
+package com.example.transformapp;
+
+import javafx.application.Application;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.stage.Stage;
+
+public class TransformApp extends Application {
+
+    private Parent createContent() {
+        Rectangle box = new Rectangle(100, 50, Color.BLUE);
+
+        transform(box);
+
+        return new Pane(box);
+    }
+
+    private void transform(Rectangle box) {
+        // we will apply transformations here
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.setScene(new Scene(createContent(), 300, 300, Color.GRAY));
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
